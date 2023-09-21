6a3d05e333314c310c600358aed0b2071fdbeead
andallves <andrealves10a@gmail.com>
2023-09-21 17:02:29 -0300

    Fix some bug

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -2,7 +2,5 @@ import logo from '../../assets/img/logo.svg'
 import './styles.css'
 
 export const Logo = () => {
-  return (
-    <img className='logo' src={logo} alt='logo' />
-  )
+  return <img className='logo' src={logo} alt='logo' />
 }
