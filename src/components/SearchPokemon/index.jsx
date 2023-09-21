2938e456d30a4b7826a093426b3c41dbf872dba3
andallves <andrealves10a@gmail.com>
2023-09-21 17:03:59 -0300

    Implement new functionality in SearchPokemon

--- src/components/SearchPokemon/index.jsx
+++ src/components/SearchPokemon/index.jsx
@@ -1,10 +1,27 @@
+import PropTypes from 'prop-types'
 import './styles.css'
 
-export const SearchPokemon = () => {
+export const SearchPokemon = ({ textLabel, handleChange, searchValue, placeholder }) => {
     return (
-        <div>
-            <label>Select your Pokémon</label>
-            <input type='text' className='searchPokemon' placeholder='Search name or code'/>
+        <div className='container-input-search'>
+            <label className='label-search-pokemon' htmlFor='input-search'>
+                {textLabel}
+            </label>
+            <input 
+                className='input-search-pokemon' 
+                onChange={handleChange}
+                type='search' 
+                value={searchValue}
+                placeholder={placeholder}
+                name='input-search'
+            />
         </div>
     )
+}
+
+SearchPokemon.propTypes = {
+    textLabel: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    searchValue: PropTypes.string.isRequired,
 }
