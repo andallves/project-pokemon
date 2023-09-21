5b8f170428f8404b09b501e27f7c4a293980a100
andallves <andrealves10a@gmail.com>
2023-09-21 13:28:13 -0300

    Add Navbar e Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './layout/Header'
-import { SearchPokemon } from './components/SearchPokemon'
+import { Navbar } from './layout/Navbar'
+
 
 function App() {
 
   return (
     <main>
+      <Navbar />
       <Header />
-      <SearchPokemon />
+      
     </main>
   )
 }
