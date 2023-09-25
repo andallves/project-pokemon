4db420078c36eb8bbd3f1ba0617777113f7c8c88
andallves <andrealves10a@gmail.com>
2023-09-25 14:15:14 -0300

    Update layout CardPokemon component

--- src/components/CardPokemon/index.jsx
+++ src/components/CardPokemon/index.jsx
@@ -1,33 +1,64 @@
 import PropTypes from 'prop-types'
 import './styles.css'
+import { useEffect, useRef, useState } from 'react'
+import GrassType from '../../assets/img/icon-types/grass.svg'
 
-export const CardPokemon = ({ srcImg, pokemonName, pokemonId, pokemonType, srcImgType }) => {
+export const CardPokemon = ({ urlPokemon }) => {
+    const [pokemonData, setPokemonData] = useState({})
+    const isMounted = useRef(true)
+
+    useEffect(() => {
+        const load = async () => {
+            try{
+                const data = await fetch(`${urlPokemon}`)
+                const json = await data.json()
+                setPokemonData(json)
+            } catch (err) {
+                setPokemonData(undefined)
+                throw new Error('Erro', err)
+            }
+        }
+
+        if (isMounted.current === true) {
+            load()
+        }
+
+        return () =>{
+            isMounted.current = false
+        }
+    }, [urlPokemon])
+
+
+    const getPokemonType = (pokemonType) => {
+        switch(pokemonType) {
+            case "grass":
+                return GrassType
+        }
+        return console.log('err')
+    }
+
     return (
-       <div className="card-pokemon">
+       <button className="card-pokemon">
             <div className="container-image">
                 <img   
                     className="pokemon-image" 
-                    src={srcImg} 
-                    alt={pokemonName} 
+                    src={pokemonData?.sprites?.front_default} 
+                    alt={pokemonData.name} 
                 />
             </div>
             <div className="container-title">
                 <div className="pokemon-title">
-                    <span>{pokemonId}</span>
-                    <h3>{pokemonName}</h3>
+                    <span>{`#00${pokemonData.id}`}</span>
+                    <h3>{pokemonData.name}</h3>
                 </div>
                 <div className="pokemon-type">
-                    <img src={srcImgType} alt="" />
-                </div>
+                    <img src={() => getPokemonType('grass')} alt="" />
+                </div> 
             </div>
-       </div> 
+       </button> 
     )
 }
 
 CardPokemon.propTypes = {
-    srcImg: PropTypes.string.isRequired, 
-    pokemonName: PropTypes.string.isRequired, 
-    pokemonId: PropTypes.string.isRequired, 
-    pokemonType: PropTypes.string.isRequired, 
-    srcImgType: PropTypes.string.isRequired,
+    urlPokemon: PropTypes.string.isRequired, 
 }
