763a863b13f6f7268a8d8b7ca1284620dd0b5a15
andallves <andrealves10a@gmail.com>
2023-09-26 00:54:53 -0300

    fix bug in PokemonCard component

--- src/components/CardPokemon/index.jsx
+++ src/components/CardPokemon/index.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import './styles.css'
 import { useEffect, useRef, useState } from 'react'
+import { getPokemonType } from '../../utils/get-type-name'
 // import GrassType from '../../assets/img/icon-types/grass.svg'
 
 export const CardPokemon = ({ urlPokemon }) => {
@@ -8,11 +9,20 @@ export const CardPokemon = ({ urlPokemon }) => {
     const isMounted = useRef(true)
 
     useEffect(() => {
-        const load = async () => {
+        const loadCard = async () => {
             try{
                 const data = await fetch(`${urlPokemon}`)
                 const json = await data.json()
-                setPokemonData(json)
+                const { name, id, sprites, types } = json
+
+                const infoCard = {
+                    name: name,
+                    code: id,
+                    image: sprites.other.dream_world.front_default,
+                    type: types[0].type.name,
+                }
+
+                setPokemonData(infoCard)
             } catch (err) {
                 setPokemonData(undefined)
                 throw new Error('Erro', err)
@@ -20,7 +30,7 @@ export const CardPokemon = ({ urlPokemon }) => {
         }
 
         if (isMounted.current === true) {
-            load()
+            loadCard()
         }
 
         return () =>{
@@ -28,31 +38,24 @@ export const CardPokemon = ({ urlPokemon }) => {
         }
     }, [urlPokemon])
 
-
-    const getPokemonType = (pokemonType) => {
-        switch(pokemonType) {
-            case "grass":
-                return "grass"
-        }
-        return console.log('err')
-    }
+    const typeImg = getPokemonType(pokemonData.type)
 
     return (
-       <button className="card-pokemon">
-            <div className="container-image">
+       <button className={`card-pokemon ${pokemonData.type}`}>
+            <div className="image">
                 <img   
-                    className="pokemon-image" 
-                    src={pokemonData?.sprites?.front_default} 
+                    className="thumb-img" 
+                    src={pokemonData?.image} 
                     alt={pokemonData.name} 
                 />
             </div>
-            <div className="container-title">
-                <div className="pokemon-title">
-                    <span>{`#00${pokemonData.id}`}</span>
+            <div className="info"> 
+                <div className="text">
+                    <span>{`#00${pokemonData.code}`}</span>
                     <h3>{pokemonData.name}</h3>
                 </div>
-                <div className="pokemon-type">
-                    <img src={() => getPokemonType('grass')} alt="" />
+                <div className="icon-type">
+                    <img src={typeImg} alt={pokemonData.type} />
                 </div> 
             </div>
        </button> 
