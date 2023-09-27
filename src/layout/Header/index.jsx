dc3de20e700ae65c4f2ffd2340ea88f7e0ec9111
andallves <andrealves10a@gmail.com>
2023-09-27 14:58:36 -0300

    Add funcionality to change header

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,21 +1,38 @@
-import pokeball from '../../assets/img/pokeball-red.png'
+import pokeballRed from '../../assets/img/pokeball-red.png'
+import pokeballBlue from '../../assets/img/pokeball-blue.png'
 
 import './styles.css'
 import { Navbar } from '../Navbar'
 import { Heading } from '../../components/Heading'
+import { useState } from 'react'
 
 export const Header = () => {
+    const [toggle, setToggle] = useState(false)
+    setTimeout(() => {
+        setToggle(!toggle)
+    }, 10000)
+
     return (
         <header>
-            <div className='container'>
+            <div className={`container-header ${toggle ? "bg-red" : "bg-blue"}`}>
                 <Navbar />
-
-                <Heading
-                    title={'Who is that Pokémon?'}
-                    subtitle={'The perfect guide for those who want to hunt Pokémons around the world'}
-                />
-
-                <img className='pokeball' src={pokeball} alt='pokeball-red'/>
+                {toggle ? (
+                    <Heading
+                        title={'Who is that Pokémon?'}
+                        subtitle={'The perfect guide for those who want to hunt Pokémons around the world'}
+                    />
+                ) : (
+                    <Heading
+                        title={'Catch the all!'}
+                        subtitle={'The perfect guide for those who want to hunt Pokémons around the world'}
+                    />
+                )}
+                
+                {toggle ? (
+                    <img className='pokeball' src={pokeballRed} alt='pokeball-red'/>
+                ) : (
+                    <img className='pokeball blue' src={pokeballBlue} alt='pokeball-blue'/>
+                )}
             </div>
         </header>
     )
