be27c6b7ec4f387155da58c289805d3b6fa78275
andallves <andrealves10a@gmail.com>
2023-09-27 14:34:48 -0300

    fixing bug in Card component

--- src/components/CardPokemon/index.jsx
+++ src/components/CardPokemon/index.jsx
@@ -1,82 +1,83 @@
 import PropTypes from 'prop-types'
 import './styles.css'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getPokemonTypeImage } from '../../utils/get-type-image'
 import { ModalDetails } from '../ModalDetails'
+import { capitalizeFirstLetter } from '../../utils/captitaliza-first-letter'
 
 export const CardPokemon = ({ urlPokemon }) => {
-    const [pokemonData, setPokemonData] = useState({})
-    const [open, setOpen] = useState(false)
-    const handleOpen = () => setOpen(true)
-    const handleClose = () => setOpen(false)
-    const isMounted = useRef(true)
+  const [pokemonData, setPokemonData] = useState({})
+  const [open, setOpen] = useState(false)
 
-    useEffect(() => {
-        const loadCard = async () => {
-            try{
-                const data = await fetch(`${urlPokemon}`)
-                const json = await data.json()
-                const { abilities, height, name, id, sprites, stats, types, weight } = json
+  useEffect(() => {
+    const loadCard = async () => {
+      try {
+        const data = await fetch(`${urlPokemon}`)
+        const json = await data.json()
+        const { abilities, height, id, name, sprites, stats, types, weight } = json
 
-                const infoCard = {
-                    abilities: abilities[0].name,
-                    height: height,
-                    name: name,
-                    code: id,
-                    image: sprites.other.dream_world.front_default,
-                    stats: stats,
-                    type: types[0].type.name,
-                    weight: weight,
-                }
-
-                setPokemonData(infoCard)
-            } catch (err) {
-                setPokemonData(undefined)
-                throw new Error('Erro', err)
-            }
+        const weaknessesResponse = await fetch(types[0].type.url)
+        const weakJson = await weaknessesResponse.json()
+        
+        const { double_damage_from } = weakJson.damage_relations
+        const infoCard = {
+          abilities: abilities[0].ability.name,
+          height: height,
+          code: id,
+          name: name,
+          image: sprites.other.dream_world.front_default,
+          stats: stats,
+          weak: double_damage_from,
+          types: types,
+          type: types[0].type.name,
+          weight: weight,
         }
+        setPokemonData(infoCard)
+      } catch (err) {
+        setPokemonData(undefined)
+        throw new Error('Erro', err)
+      }
+    }
+    loadCard()
+  }, [urlPokemon])
 
-        if (isMounted.current === true) {
-            loadCard()
-        }
+  const typeImg = getPokemonTypeImage(pokemonData.type)
+  const handleOpenCloseModal = () => setOpen(false)
 
-        return () =>{
-            isMounted.current = false
-        }
-    }, [urlPokemon])
+  return (
+    <>
+      <button
+        className={`card-pokemon ${pokemonData.type}`}
+        onClick={() => setOpen(true)}
 
-    const typeImg = getPokemonTypeImage(pokemonData.type)
+      >
+        <div className="image">
+          <img
+            className="thumb-img"
+            src={pokemonData.image}
+            alt={pokemonData.name}
+          />
+        </div>
+        <div className="info">
+          <div className="text">
+            <span>{`#00${pokemonData.code}`}</span>
+            <h3>{capitalizeFirstLetter(pokemonData.name)}</h3>
+          </div>
+          <div className="icon-type">
+            <img src={typeImg} alt={pokemonData.type} />
+          </div>
+        </div>
+      </button>
 
-    return (
-        <>
-            <button 
-                className={`card-pokemon ${pokemonData.type}`}
-                onClick={handleOpen}
-                
-            >
-                <div className="image">
-                    <img   
-                        className="thumb-img" 
-                        src={pokemonData.image} 
-                        alt={pokemonData.name} 
-                    />
-                </div>
-                <div className="info"> 
-                    <div className="text">
-                        <span>{`#00${pokemonData.code}`}</span>
-                        <h3>{pokemonData.name}</h3>
-                    </div>
-                    <div className="icon-type">
-                        <img src={typeImg} alt={pokemonData.type} />
-                    </div> 
-                </div>
-            </button> 
-
-            <ModalDetails pokemonData={pokemonData} isOpen={open} btnClose={handleClose} />
-       </>
-    )
+      <ModalDetails 
+        pokemonData={pokemonData} 
+        isOpen={open} 
+        btnClose={handleOpenCloseModal} 
+        />
+    </>
+  )
 }
 
 CardPokemon.propTypes = {
-    urlPokemon: PropTypes.string.isRequired, 
+  urlPokemon: PropTypes.string.isRequired,
 }
